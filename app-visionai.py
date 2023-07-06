d3ec3cf76c8df051ad7c578a013ce7eed0b715bb
Muhammad Muzammil <muzammilpervaiz925139@gmail.com>
2023-07-06 17:10:30 +0500

    Update app-visionai.py

--- app-visionai.py
+++ app-visionai.py
@@ -183,59 +183,34 @@ def google_pdf_ocr(file_path, mime_type):
 import pandas as pd
 import os
 
+
+
 def csvWriter(refinedText, output_file):
     extracted_dict = eval(refinedText)
     df = pd.DataFrame([extracted_dict])
 
     if os.path.isfile(output_file):
-        os.remove(output_file)  # remove the old file before creating a new one
+        existing_df = pd.read_csv(output_file)
+        df = pd.concat([existing_df, df], ignore_index=True)
+    else:
+        columns_order = ["Supplier Name", "Customer Name", "TRN of Supplier", "TRN of Customer",
+                         "Address of Supplier", "Address of Customer", "Date of Invoice", "Invoice Number",
+                         "Payment due date", "Description of goods / services ", "Net Amount ", "VAT Amount ",
+                         "Total Amount ", "Cross validated total amount ", "Email for correspondence",
+                         "Tax Invoice ", "Tax "]
 
-    columns_order = ["Supplier Name", "Customer Name", "TRN of Supplier", "TRN of Customer",
-                     "Address of Supplier", "Address of Customer", "Date of Invoice", "Invoice Number",
-                     "Payment due date", "Description of goods / services ", "Net Amount ", "VAT Amount ",
-                     "Total Amount ", "Cross validated total amount ", "Email for correspondence",
-                     "Tax Invoice ", "Tax "]
+        # Ensure all necessary columns exist in the DataFrame
+        for col in columns_order:
+            if col not in df.columns:
+                df[col] = None
 
-    # Ensure all necessary columns exist in the DataFrame
-    for col in columns_order:
-        if col not in df.columns:
-            df[col] = None
-
-    # Reorder the columns
-    df = df[columns_order]
+        # Reorder the columns
+        df = df[columns_order]
 
     # Remove all columns in which all values are None or NaN
     df = df.dropna(how='all', axis=1)
 
     df.to_csv(output_file, index=False)
-
-
-# def csvWriter(refinedText, output_file):
-#     extracted_dict = eval(refinedText)
-#     df = pd.DataFrame([extracted_dict])
-
-#     if os.path.isfile(output_file):
-#         existing_df = pd.read_csv(output_file)
-#         df = pd.concat([existing_df, df], ignore_index=True)
-#     else:
-#         columns_order = ["Supplier Name", "Customer Name", "TRN of Supplier", "TRN of Customer",
-#                          "Address of Supplier", "Address of Customer", "Date of Invoice", "Invoice Number",
-#                          "Payment due date", "Description of goods / services ", "Net Amount ", "VAT Amount ",
-#                          "Total Amount ", "Cross validated total amount ", "Email for correspondence",
-#                          "Tax Invoice ", "Tax "]
-
-#         # Ensure all necessary columns exist in the DataFrame
-#         for col in columns_order:
-#             if col not in df.columns:
-#                 df[col] = None
-
-#         # Reorder the columns
-#         df = df[columns_order]
-
-#     # Remove all columns in which all values are None or NaN
-#     df = df.dropna(how='all', axis=1)
-
-#     df.to_csv(output_file, index=False)
 
 
 def get_binary_file_downloader_html(bin_file, file_label='File'):
@@ -367,6 +342,8 @@ def main():
 
     # Process button
     if st.button('Process'):
+        if os.path.isfile(output_file):
+            os.remove(output_file)  # remove the old file before creating a new one
         with st.spinner("Performing OCR..."):
 
             # Ensure files have been uploaded
